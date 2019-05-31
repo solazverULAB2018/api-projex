192fb9e78ac0cdbb0db10e520e1c2bc15b539c0c
Julio <jmanuellugo96@gmail.com>
2019-05-31 16:00:41 -0400

    Prepared for first deployment

--- projexbackend/urls.py
+++ projexbackend/urls.py
@@ -17,8 +17,6 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.urls import path
 from rest_framework_swagger.views import get_swagger_view
-from rest_framework import routers
-
 
 # Swagger documentation
 schema_view = get_swagger_view(title='Pastebin API')
