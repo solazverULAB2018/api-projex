8bb8d31f875ca1211a6f55d2750c3427b737ced2
JennyRemolina <remojenny@gmail.com>
2019-07-09 12:34:40 -0400

    change in user registration and validation email

--- users/tests/test_registration.py
+++ users/tests/test_registration.py
@@ -3,26 +3,26 @@ import json
 from io import StringIO
 from unittest.mock import patch
 
-from django.contrib.auth.models import User
-from django.urls import reverse
-
 from rest_framework.test import APITestCase
 
 
 class UserRegistrationAPIViewTestCase(APITestCase):
     url = '/api/v1/rest-auth/registration/'
+    username = "testuser"
+    email = "test@testuser.com"
+    password = "12345Hola"
 
     def setUp(self):
         pass
 
-    def test_invalid_password1(self):
+    def test_invalid_password_unmatch(self):
         """
         Test to verify that a post call with invalid(unmatch) passwords
         """
         data = {
-            "username": "testuser",
-            "email": "test@testuser.com",
-            "password1": "password",
+            "username": self.username,
+            "email":    self.email,
+            "password1": "passwordStrong",
             "password2": "INVALID_PASSWORD",
             "country": "Venezuela",
             #"profile_photo": "",
@@ -30,30 +30,32 @@ class UserRegistrationAPIViewTestCase(APITestCase):
         response = self.client.post(self.url, data, format='json')
         print(response.status_code, response.content)
         self.assertEqual(400, response.status_code)
+        self.assertTrue("password1" in json.loads(response.content))
 
-    def test_invalid_password2(self):
+    def test_invalid_password_short(self):
         """
         Test to verify that a post call with invalid(short) passwords
         """
         data = {
-            "username": "testuser",
-            "email": "test@testuser.com",
-            "password1": "1234567",
-            "password2": "1234567",
+            "username": self.username,
+            "email":    self.email,
+            "password1": "123Hola",
+            "password2": "123Hola",
             "country": "Venezuela",
             #"profile_photo": "",
         }
         response = self.client.post(self.url, data, format='json')
         print(response.status_code, response.content)
         self.assertEqual(400, response.status_code)
+        self.assertTrue("password1" in json.loads(response.content))
 
-    def test_invalid_password3(self):
+    def test_invalid_password_simple(self):
         """
         Test to verify that a post call with invalid(simple) passwords
         """
         data = {
-            "username": "testuser",
-            "email": "test@testuser.com",
+            "username": self.username,
+            "email":    self.email,
             "password1": "12345678",
             "password2": "12345678",
             "country": "Venezuela",
@@ -62,16 +64,17 @@ class UserRegistrationAPIViewTestCase(APITestCase):
         response = self.client.post(self.url, data, format='json')
         print(response.status_code, response.content)
         self.assertEqual(400, response.status_code)
+        self.assertTrue("password1" in json.loads(response.content))
 
     def test_user_registration(self):
         """
         Test to verify that a post call with user valid data
         """
         data = {
-            "username": "testuser",
-            "email": "test@testuser.com",
-            "password1": "123aT678",
-            "password2": "123aT678",
+            "username":  self.username,
+            "email":     self.email,
+            "password1": self.password,
+            "password2": self.password,
             "country": "Venezuela",
             #"profile_photo": "",
         }
@@ -87,10 +90,10 @@ class UserRegistrationAPIViewTestCase(APITestCase):
         """
         #mock_save.return_value = 'photo.jpg'
         data = {
-            "username": "testuser",
-            "email": "test@testuser.com",
-            "password1": "123aT678",
-            "password2": "123aT678",
+            "username":  self.username,
+            "email":     self.email,
+            "password1": self.password,
+            "password2": self.password,
             "country": "Venezuela",
             #"profile_photo": StringIO('test'),
         }
@@ -104,10 +107,10 @@ class UserRegistrationAPIViewTestCase(APITestCase):
         Test to verify that a post call with already exists username
         """
         user1 = {
-            "username": "testuser",
-            "email": "test@testuser.com",
-            "password1": "123aT678",
-            "password2": "123aT678",
+            "username":  self.username,
+            "email":     self.email,
+            "password1": self.password,
+            "password2": self.password,
             "country": "Venezuela",
         }
         response = self.client.post(self.url, user1)
@@ -115,12 +118,40 @@ class UserRegistrationAPIViewTestCase(APITestCase):
         self.assertEqual(201, response.status_code)
 
         user2 = {
-            "username": "testuser",
+            "username":  self.username,
             "email": "test2@testuser.com",
-            "password1": "123aT678",
-            "password2": "123aT678",
+            "password1": self.password,
+            "password2": self.password,
             "country": "Venezuela",
         }
         response = self.client.post(self.url, user2)
         print(response.status_code, response.content)
         self.assertEqual(400, response.status_code)
+        self.assertTrue("username" in json.loads(response.content))
+
+    def test_unique_email_validation(self):
+        """
+        Test to verify that a post call with already exists email
+        """
+        user1 = {
+            "username":  self.username,
+            "email":     self.email,
+            "password1": self.password,
+            "password2": self.password,
+            "country": "Venezuela",
+        }
+        response = self.client.post(self.url, user1)
+        print(response.status_code, response.content)
+        self.assertEqual(201, response.status_code)
+
+        user2 = {
+            "username":  'testuser2',
+            "email":     self.email,
+            "password1": self.password,
+            "password2": self.password,
+            "country": "Venezuela",
+        }
+        response = self.client.post(self.url, user2)
+        print(response.status_code, response.content)
+        self.assertEqual(400, response.status_code)
+        self.assertTrue("email" in json.loads(response.content))
