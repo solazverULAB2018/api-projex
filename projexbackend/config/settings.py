cbb52646696f8f270d5a8fc6d6534ee36c0f877a
Julio <jmanuellugo96@gmail.com>
2019-07-10 22:26:27 -0400

    Enable session authentication for tests purposes

--- projexbackend/config/settings.py
+++ projexbackend/config/settings.py
@@ -181,7 +181,8 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.TokenAuthentication',s
+        'rest_framework.authentication.SessionAuthentication'
     ),
     'TEST_REQUEST_DEFAULT_FORMAT': 'json'
 
