cfe35fa4620e4d3447d094d05079bf914c074463
Julio <jmanuellugo96@gmail.com>
2019-07-15 01:55:52 -0400

    Testing hidding line

--- projexbackend/config/settings.py
+++ projexbackend/config/settings.py
@@ -28,7 +28,7 @@ STATICFILES_DIRS = (
 )
 
 #  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Application definition
 
